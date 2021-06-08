f441bf098e712bf32fd4aa322131bf3e9c0d8a23
Postuma <maarten.postuma@wur.nl>
2021-06-08 16:33:07 +0200

    Added loading data into methylKit, filtering data in methylKit and running basic analysis
    Basic analysis of percentage methylation between treatments and PCA

---
+++ src/basicAnalysisMethylKit.R
@@ -0,0 +1,64 @@
+rm(list=ls())
+library(methylKit)
+library(ggplot2)
+library(reshape2)
+library(ggpubr)
+colorBlindBlack8  <- c("#000000", "#E69F00", "#56B4E9", "#009E73", 
+                       "#F0E442", "#0072B2", "#D55E00", "#CC79A7") #Make pretty colours
+#################
+#performs basic percentage methylation and PCA analysis
+#Make sure to change variables specifying colour and shape in plots to match the variables
+#In your own Design table
+#################
+
+#Loads the filtered design
+
+Design<-read.table("results/filtering/Design_filtered.tsv")
+
+
+i<-0
+percMethData<-pcaPlot<-list()
+#Reads the united data for each different context
+for (context in c("CG","CHG","CHH")){
+  i<-i+1
+  united<-readMethylDB(list.files(paste0("/scratch2/maarten/scabiosa/Filt",context),
+                                  pattern = paste0("methylBase_",context,".txt.bgz$")
+                                  ,full.names=T)
+                       )
+##########################################  
+  #PCA's for each context
+  pca<-PCASamples(united,scale = F,obj.return = T,screeplot = F) #create the PCA
+  
+  pcaPerc<-round(pca$sdev/sum(pca$sdev)*100,1) #Calculate percentage of variante per PC
+  pcaData<-data.frame(PC1=pca$x[,1],PC2=pca$x[,2],PC3=pca$x[,3],indName=row.names(pca$x))
+  pcaPlotData<-merge(pcaData,Design,by="indName")
+  pcaPlot[[i]]<-ggplot(pcaPlotData,aes(x=PC1,y=PC2,col=site,shape=treatment.y))+
+    geom_point(size=3)+theme_light()+
+    scale_colour_manual(values = colorBlindBlack8)+ggtitle(context)+
+    xlab(paste0("PC1 (",pcaPerc[1],"%)"))+
+    ylab(paste0("PC2 (",pcaPerc[2],"%)"))
+###########################################
+  #Percentage methylation dataframes
+  percMethData[[i]]<-as.data.frame(t(percMethylation(united)))
+  
+  
+}
+
+g <- ggarrange(pcaPlot[[1]],pcaPlot[[2]],pcaPlot[[3]], ncol=3, nrow=1, widths=c(6,6), align="h", labels="AUTO", common.legend = TRUE, legend="right", legend.grob=get_legend(pcaPlot[[1]]))
+g
+ggsave("results/pca.png",g,height=4.5,width=9,dpi="retina")
+
+
+#Combine the percentage methylation with the Design.
+percMethDataOut<-data.frame(indName=row.names(percMethData[[1]]),
+                         CG=rowMeans(percMethData[[1]],na.rm=T),
+                         CHG=rowMeans(percMethData[[2]],na.rm=T),
+                         CHH=rowMeans(percMethData[[3]],na.rm=T))
+meltPlot<-melt(percMethDataOut)
+percMethPlot<-merge(meltPlot,Design,by="indName")
+
+ggplot(percMethPlot,aes(x=treatment.y,y=value,col=site))+geom_boxplot()+facet_grid(.~variable,scales="free",space = "free")+ylab("percentage methylation")+
+  scale_colour_manual(values = colorBlindBlack8)+theme_light()+
+  ggsave("results/percMeth.png")
+
+
---
+++ src/filterMethylKit.R
@@ -0,0 +1,105 @@
+library(methylKit)
+library(ggplot2)
+library(reshape2)
+library(ggpubr)
+#########################################
+
+#Takes output from the readMethylKit + a design table:
+#The Design table should contain a column called sampleID with the names of the samples (same as the names used in epiGBS) and can contain all other treatments etc. These will be combined with nLoci data after filtering to create filtered design. Which you can use to determine what filtering steps leave enough samples per treatment group
+#
+
+
+##############################################
+nLociData<-read.table("results/filtering/nLociPerInd.tsv") #Load nLoci per sample generated previously
+Design<-read.table("data/Design.tsv") #Load design file
+
+#############################################
+
+
+filterCutOff<-50000 #Filtering cut off. Can be anything from a number of loci (in this case)
+                                       #or based on the max / mean / min etc. 
+                                       #I Haven't figured out what works best yet.
+ind_miss<-0.8            #Fraction of individuals a 
+
+
+#####################
+
+#Plots where the filtering cut off occurs and how many individuals would get filtered
+#Check output location of the plot
+ggplot(nLociData,aes(x=nloci))+geom_histogram(binwidth = 5000,fill="white",col="black")+
+  theme_classic()+
+  geom_vline(xintercept = filterCutOff,col="red",size=2)+
+  ggtitle(paste("n Individuals filtered =",
+                length(nLociData$nloci[nLociData$nloci<filterCutOff]),"out of",
+                length(nLociData[,1])))+
+  ggsave("results/filtering/nLociDistrib.png")
+#################################
+
+
+
+nLociDataFiltFinal<-nLociData[nLociData$nloci>filterCutOff,] #Filters the samples that don't meet the                                                               filtering criteria
+
+#You can add other filtering steps here. In this experiment every sample was twice. So we only kept samples which passed filters twice
+nLociDataFiltFinal<-nLociDataFiltFinal[nLociDataFiltFinal$indNumber%in%names(which(table(nLociDataFiltFinal$indNumber) > 1)),]
+
+
+
+
+
+##########################################################
+merged<-merge(nLociDataFiltFinal,Design,by.x="indName",by.y="sampleID") #Filters the design
+
+
+
+aggregate(nloci~site+treatment.y,
+          merged,length) #Creates a table with the number of individuals                                                        passing filters per experimental treatment,                                                           need to change the treatment into the column                                                          names in your design file
+
+write.table(merged,"results/filtering/Design_filtered.tsv") #writes the filtered Design
+
+#################
+#Create the methylKit databases for each different context
+methylationFilesCG<-methRead(location = as.list(nLociDataFiltFinal$loc),
+                             as.list(nLociDataFiltFinal$indName),
+                             assembly = "arabidopsisRef",
+                             pipeline = "bismarkCytosineReport",
+                             treatment = rep(1,times=length(nLociDataFiltFinal$nloci)),
+                             mincov = 10,
+                             dbdir = "FiltCG")
+methylDBCG<-makeMethylDB(methylationFilesCG,dbdir = "FiltCG")
+
+normalizedCG<-normalizeCoverage(methylDBCG,dbdir="FiltCG")
+unitedCG<-unite(normalizedCG,min.per.group=as.integer(length(nLociDataFiltFinal$loc)*ind_miss),suffix="CG")
+
+
+
+methylationFilesCHG<-methRead(location = as.list(nLociDataFiltFinal$loc),
+                              as.list(nLociDataFiltFinal$indName),
+                              assembly = "arabidopsisRef",
+                              pipeline = "bismarkCytosineReport",
+                              treatment = rep(1,times=length(nLociDataFiltFinal$nloci)),
+                              mincov = 10,context="CHG",
+                              dbdir = "FiltCHG")
+methylDBCHG<-makeMethylDB(methylationFilesCHG,dbdir = "FiltCHG")
+
+normalizedCHG<-normalizeCoverage(methylDBCHG,dbdir="FiltCHG")
+unitedCHG<-unite(normalizedCHG,
+                 min.per.group=as.integer(length(nLociDataFiltFinal$loc)*ind_miss),suffix="CHG")
+
+
+
+# unitedCHG<-readMethylDB("/scratch2/maarten/scabiosa/FiltCHG/methylBase_2311ef4affd682.txt.bgz")
+
+
+# 
+methylationFilesCHH<-methRead(location = as.list(nLociDataFiltFinal$loc),
+                              as.list(nLociDataFiltFinal$indName),
+                              assembly = "arabidopsisRef",
+                              pipeline = "bismarkCytosineReport",
+                              treatment = rep(1,times=length(nLociDataFiltFinal$nloci)),
+                              mincov = 10,context="CHH",
+                              dbdir = "FiltCHH")
+methylDBCHH<-makeMethylDB(methylationFilesCHH,dbdir = "FiltCHH")
+
+normalizedCHH<-normalizeCoverage(methylDBCHH,dbdir="FiltCHH")
+unitedCHH<-unite(normalizedCHH,min.per.group=as.integer(length(nLociDataFiltFinal$loc)*ind_miss),suffix="CHH")
+
